f39ea1f6a128e76e99da58e81158198a6c76fa65
Eric <em13914@users.noreply.github.com>
2025-05-06 12:14:01 -0700

    Update TurnBlack.cs
    Updated indentions and comments.

--- TurnBlack.cs
+++ TurnBlack.cs
@@ -5,17 +5,13 @@ public class TurnBlack : MonoBehaviour
     void Start()
     {
         // print at the beginning so we know the script is running
-        UnityEngine.Debug.Log("Starting...");
+	UnityEngine.Debug.Log("Starting...");
     }
 
-    void Update()
+    void OnCollisionEnter(Collision other)
     {
-        
-    }
-	
-	void OnCollisionEnter(Collision other)
-	{
+        // print when there's a collision so we know the script recognizes hits
         UnityEngine.Debug.Log("HIT!!!!!!!!!!!!!!!!");
-        GetComponent<Renderer>().material.color = Color.black;
-	}
+	GetComponent<Renderer>().material.color = Color.black;
+    }
 }
