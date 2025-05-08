f751cfc65e1ff450adbaee397a7c119c71d295c3
Eric <em13914@users.noreply.github.com>
2025-05-07 20:35:24 -0700

    Update TurnBlackCollision.cs

--- TurnBlackCollision.cs
+++ TurnBlackCollision.cs
@@ -1,17 +1,16 @@
 using UnityEngine;
 
-public class TurnBlack : MonoBehaviour
+public class TurnBlackCollision : MonoBehaviour
 {
     void Start()
     {
         // print at the beginning so we know the script is running
-	UnityEngine.Debug.Log("Starting...");
+        UnityEngine.Debug.Log("Starting...");
     }
-
-    void OnCollisionEnter(Collision other)
-    {
-        // print when there's a collision so we know the script recognizes hits
-        UnityEngine.Debug.Log("HIT!!!!!!!!!!!!!!!!");
-	GetComponent<Renderer>().material.color = Color.black;
-    }
+	
+	void OnCollisionEnter(Collision other)
+	{
+        // find the object this script is attached to and change the color to black
+        GetComponent<Renderer>().material.color = Color.black;
+	}
 }
