f3abb9e93853dbf4afb964fdafc7360999831a53
Eric <em13914@users.noreply.github.com>
2025-05-13 18:52:12 -0700

    Update ApplyForce.cs

--- ApplyForce.cs
+++ ApplyForce.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 public class ApplyForce : MonoBehaviour
 {
-    public Vector3 velocity = new Vector3(0f, 0f, -2f); // Set your direction and speed here
+    // Choose a direction and speed for the force you want to apply
+    private Vector3 force = new Vector3(0f, 0f, 0f); // Set your direction and speed here
 
     void FixedUpdate()
     {
         // Set velocity directly every physics update (ignores mass or physics forces)
-        GetComponent<Rigidbody>().linearVelocity = velocity;
+        GetComponent<Rigidbody>().linearVelocity = force;
     }
 }
